5088357ddade8a000993cb54b2f9acec09a3ef5b
Muhammad Umer <muhammad.umer@pakistancables.com>
2026-05-30 02:59:00 +0500

    updated admin

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,8 +12,10 @@ const allowedOrigins = new Set([
   config.clientUrl,
   'http://localhost:5173',
   'http://localhost:5174',
+  'http://localhost:5175',
   'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174'
+  'http://127.0.0.1:5174',
+  'http://127.0.0.1:5175'
 ]);
 
 app.use(
